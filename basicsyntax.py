ed9b5e824156589fb6d8256627ca3f5e89bd12cd
Safety Vest 2789 <jrnlife@gmail.com>
2022-08-18 17:02:06 -0400

    some more syntax problems

---
+++ basicsyntax.py
@@ -0,0 +1,11 @@
+"banananaanananana"
+"banananaanananana".upper()
+
+type("hey words")
+
+print(bool(int("1")))
+
+Good="GsoAo+d"
+Good[::2]
+
+#you end up with "Good" every other even number, or every 2 index place
