73c712bb3f12eade0f7edce6f025196991e97a00
Safety Vest 2789 <jrnlife@gmail.com>
2022-08-19 15:55:50 -0400

    strings and logical operators

---
+++ strings.py
@@ -0,0 +1,9 @@
+#string methods and syntax examples using replace and "in" keyword
+course = 'Python go BrrRrrR'
+print(course.replace('go', '4'))
+
+
+
+#logical operators
+price = 25
+print(price > 10 and price < 30)
