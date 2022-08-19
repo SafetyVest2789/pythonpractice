1593a0f3e2d99c3c92c315fd72cf6805f3493035
Safety Vest 2789 <jrnlife@gmail.com>
2022-08-19 16:07:53 -0400

    eleeeeeeeeif

---
+++ elifstatements.py
@@ -0,0 +1,12 @@
+temperature = 25
+
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print ("It's a nice day")
+elif temperature > 10: 
+    print("it's getting cold")
+else:
+    print("it's BRRrrR")
+print("Done")   
--- strings.py
+++ strings.py
@@ -5,5 +5,8 @@ print(course.replace('go', '4'))
 
 
 #logical operators
+# -and (both)
+# -or (at least one)
+# -not (inverses any value given) 
 price = 25
 print(price > 10 and price < 30)
