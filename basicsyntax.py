026eb757fbd47b61ad8b0e0d81265bf836f35a4d
Safety Vest 2789 <jrnlife@gmail.com>
2022-08-19 14:42:17 -0400

    type conversion int example

--- basicsyntax.py
+++ basicsyntax.py
@@ -11,6 +11,17 @@ Good[::2]
 #you end up with "Good" every other even number, or every 2 index place
 
 
+#Boolean must have capital letter to True or False or it will not use proper character
 patient_name = "John Smith"
 age = 20
 new_patient = True
+
+
+# String Concatanation, make sure you leave a space after your string to allow input space
+name = input("What is your name? ")
+print("Hello " + name)
+
+#inouting type conversions
+birth_year = input ("Enter your birth year: ")
+age = 2020 - birth_year
+print(age)
